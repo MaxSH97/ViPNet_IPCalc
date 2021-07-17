59a31aed74f3797c5a6fdca73bc61eeab3427600
Maksim Strekalovskikh <max.str97@yandex.ru>
2021-07-17 14:02:31 +0500

    Added function "get_virt_ip"

--- main.py
+++ main.py
@@ -3,6 +3,30 @@
 
 import ipaddress
 
+
+class RealIPNotInRange(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
+def get_virt_ip(real_ip, r_real_ip_s, r_real_ip_e, r_virt_ip_s):
+    real_ip = int(ipaddress.IPv4Address(real_ip))
+    r_real_ip_s = int(ipaddress.IPv4Address(r_real_ip_s))
+    r_real_ip_e = int(ipaddress.IPv4Address(r_real_ip_e))
+    r_virt_ip_s = int(ipaddress.IPv4Address(r_virt_ip_s))
+
+    d_result = r_real_ip_e - real_ip
+    r_result = r_real_ip_e - r_real_ip_s
+
+    if real_ip < r_real_ip_s or real_ip > r_real_ip_e:
+        raise RealIPNotInRange('Real IP address not in range of Real IP addresses')
+
+    r_virt_ip_e = ipaddress.IPv4Address(r_virt_ip_s + r_result)
+
+    virt_ip = ipaddress.IPv4Address(int(r_virt_ip_e) - d_result)
+    return str(virt_ip), str(r_virt_ip_e)
+
+
 real_ip = input('Real IP address: ')
 real_ip = int(ipaddress.IPv4Address(real_ip))
 
